42ba6a5592e6d7a29b9a3c59083281dfd72ac481
cybergam3r <giogio03.gr@gmail.com>
2025-01-27 12:17:02 +0100

    primo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
-
   return (
     <>
-      
+      <NavBar />
+      ciaociao
     </>
-  )
+  );
 }
 
-export default App
+export default App;
