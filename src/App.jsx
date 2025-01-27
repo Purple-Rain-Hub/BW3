79e30bf642ad3816276a855320cc1288e297bab4
vintry <oennabati3@gmail.com>
2025-01-27 21:10:24 +0100

    HomePage completata + aggiunta vari svg e img

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import CentralSection from "./components/CentralSection";
 import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <HomePage />
       <CentralSection />
 
       <Footer />
