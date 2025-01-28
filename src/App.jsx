3773dedd3024a288cbfd6e1d7ab837a7e20cacef
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-01-28 10:00:08 +0100

    sistemata app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import NavBar from "./Components/NavBar";
 import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
-import CentralSection from "./components/CentralSection";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
       <HomePage />
-      <CentralSection />
-
-      <>
-        <ProfilePage />
-        <Footer />
-      </>
-    </div>
+      <ProfilePage />
+      <Footer />
+    </>
   );
 }
 
