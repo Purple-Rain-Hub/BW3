9336add163ac9df2af859af9bfca8e048c6fae18
vintry <oennabati3@gmail.com>
2025-01-29 19:08:59 +0100

    zona off-limits, non si tocca, pericolosa

---
+++ src/components/FavoritesPage.jsx
@@ -0,0 +1,38 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { removeFromFavorites } from "../redux/action";
+
+const FavoritesPage = () => {
+  const favorites = useSelector((state) => state.favorites);
+  const dispatch = useDispatch();
+
+  return (
+    <Container className="mt-4">
+      <h2>I tuoi lavori preferiti</h2>
+      <Row>
+        {favorites.map((job) => (
+          <Col md={6} lg={4} key={job._id} className="mb-3">
+            <Card>
+              <Card.Body>
+                <Card.Title>{job.title}</Card.Title>
+                <Card.Text>
+                  <strong>Azienda:</strong> {job.company_name}
+                  <br />
+                  <strong>Categoria:</strong> {job.category}
+                </Card.Text>
+                <Button
+                  variant="danger"
+                  onClick={() => dispatch(removeFromFavorites(job._id))}
+                >
+                  Rimuovi dai preferiti
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default FavoritesPage;
