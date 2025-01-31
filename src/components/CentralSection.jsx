7d8d556ab259b2544471df752641eb3c68843a77
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-01-31 12:13:16 +0100

    tolto un picco eslint

--- src/components/CentralSection.jsx
+++ src/components/CentralSection.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { Button, Card, Form, Modal } from "react-bootstrap";
 import * as Icon from "react-bootstrap-icons";
 import ExperienceSection from "./ExperienceSection";
